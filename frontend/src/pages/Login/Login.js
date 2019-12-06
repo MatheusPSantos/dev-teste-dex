7a8a7913a30bda34642b538e7406bc641a6254da
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2019-12-06 16:28:24 -0300

    commiting updates

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -6,27 +6,28 @@ export default class Login extends Component {
     render() {
         return (
             <div className="container">
-            <img src="assets/bg.jpg" className="landingImage" />
+                <img src="assets/bg.jpg" className="landingImage" />
                 <form className="submitForm">
-                    <img src="" />
+                    <img src="assets/logo.png" className="logo" />
                     <label htmlFor="inputEmail">Email</label>
-                    <input id="inputEmail" name="inputEmail" type="email" />
+                    <input id="inputEmail" name="inputEmail" type="email" placeholder="seuemail@email.com" />
 
                     <label htmlFor="inputPassword">Password</label>
-                    <input id="inputPassword" name="inputPassword" type="password" />
+                    <input id="inputPassword" name="inputPassword" type="password" placeholder="Password" />
 
-                    <label>Mostrar a senha <input type="checkbox" /></label>
+                    <label className="showPassword">Mostrar a senha <input type="checkbox" className="showCheckbox" /></label>
 
-                    <a href="#testes">Problemas para acessar a conta?</a>
+                    <a className="accessNotWork" href="#testes">Problemas para acessar a conta?</a>
 
-                    <button>Acessar</button>
+                    <button className="accessBtn">Acessar</button>
+                    <div></div>
                     <span>ou</span>
-                    <button>Cadastrar</button>
-
-                    <a href="#">Termos de uso</a> • <a href="#">Políticas de privacidade</a>
+                    <div></div>
+                    <button className="signUpBtn">Cadastrar</button>
+                    <div className="useTerms">
+                        <a href="#">Termos de uso</a> • <a href="#">Políticas de privacidade</a>
+                    </div>
                 </form>
-
-
             </div>
         )
     }
