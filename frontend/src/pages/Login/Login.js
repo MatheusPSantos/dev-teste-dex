286c0c9a1defccb07fbdbf3c986255587f56021a
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2019-12-08 13:50:33 -0300

    interface adjustments

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -6,28 +6,35 @@ export default class Login extends Component {
     render() {
         return (
             <div className="container">
-                <img src="assets/bg.jpg" className="landingImage" />
-                <form className="submitForm">
-                    <img src="assets/logo.png" className="logo" />
-                    <label htmlFor="inputEmail">Email</label>
-                    <input id="inputEmail" name="inputEmail" type="email" placeholder="seuemail@email.com" />
+                <img src="img/assets/bg.jpg" className="landingImage" />
+                <div className="submitForm">
+                    <img src="/img/assets/logo.png" className="logo" />
+                    <form>
+                        <label id="email" htmlFor="inputEmail">Email</label>
+                        <input id="inputEmail" name="inputEmail" type="email" placeholder="seuemail@email.com" />
 
-                    <label htmlFor="inputPassword">Password</label>
-                    <input id="inputPassword" name="inputPassword" type="password" placeholder="Password" />
+                        <label id="password" htmlFor="inputPassword">Password</label>
+                        <input id="inputPassword" name="inputPassword" type="password" placeholder="Password" />
 
-                    <label className="showPassword">Mostrar a senha <input type="checkbox" className="showCheckbox" /></label>
+                        <label className="showPassword">Mostrar a senha
+                        <input type="checkbox" className="showCheckbox" />
+                            <span className="mark"></span>
+                        </label>
 
-                    <a className="accessNotWork" href="#testes">Problemas para acessar a conta?</a>
+                        <a className="accessNotWork" href="#testes">Problemas para acessar a sua conta?</a>
 
-                    <button className="accessBtn">Acessar</button>
-                    <div></div>
-                    <span>ou</span>
-                    <div></div>
-                    <button className="signUpBtn">Cadastrar</button>
-                    <div className="useTerms">
-                        <a href="#">Termos de uso</a> • <a href="#">Políticas de privacidade</a>
-                    </div>
-                </form>
+                        <button className="accessBtn">Acessar</button>
+                        <div className="line">
+                            <div></div>
+                            <span>ou</span>
+                            <div></div>
+                        </div>
+                        <button className="signUpBtn">Cadastrar</button>
+                        <div className="useTerms">
+                            <a href="#">Termos de uso</a> • <a href="#">Políticas de privacidade</a>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
